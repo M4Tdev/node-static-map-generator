88567874c7d6d3dfff9889ca1d8d8be869629f34
Mateusz Lesiuk <mateo.lesiuk@gmail.com>
2019-12-16 22:33:37 +0100

    ;

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 const StaticMaps = require('staticmaps');
 const AWS = require('aws-sdk');
 const sharp = require('sharp');
