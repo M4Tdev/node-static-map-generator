2a80a0cb2cde2da3da461c88247f188ac94af6c2
Mateusz Lesiuk <mateo.lesiuk@gmail.com>
2020-02-28 15:30:52 +0100

    New map pin

--- index.js
+++ index.js
@@ -16,34 +16,30 @@ const getDimensions = (size) => {
 		case 'small':
 			return {
 				width: 600,
-				height: 400,
+				height: 300,
 			};
 		case 'medium':
 			return {
-				width: 800,
-				height: 600,
+				width: 1000,
+				height: 500,
 			};
 		case 'large':
 			return {
-				width: 1000,
-				height: 800,
+				width: 1200,
+				height: 600,
 			};
 		default:
 			return {
 				width: 800,
-				height: 600,
+				height: 400,
 			};
 	}
 };
 
-app.get('/generate', (req, res) => {
-	const { query } = req;
-
-	const { lat, lng, size = 'medium', zoom = 13 } = query;
-
+const generateMap = async (lat, lng, size, zoom = 16) => {
 	const mapImage = `map-${lat}-${lng}-${size}.png`;
 
-	const mapDimensions = getDimensions(query.size);
+	const mapDimensions = getDimensions(size);
 
 	const mapOptions = {
 		width: mapDimensions.width,
@@ -52,49 +48,66 @@ app.get('/generate', (req, res) => {
 	};
 
 	const mapMarker = {
-		img: `${__dirname}/assets/pin.png`,
-		offsetX: 18,
-		offsetY: 54,
-		width: 36,
-		height: 54,
-		coord: [parseFloat(query.lng),parseFloat(query.lat)],
+		img: `${__dirname}/assets/map_pin.png`,
+		offsetX: 22,
+		offsetY: 16,
+		width: 32,
+		height: 44,
+		coord: [parseFloat(lng),parseFloat(lat)],
 		};
 
 	const map = new StaticMaps(mapOptions);
-	const center = [parseFloat(query.lng),parseFloat(query.lat)];
+	const center = [parseFloat(lng),parseFloat(lat)];
 
 	map.addMarker(mapMarker);
 
-	map.render(center, zoom)
-	.then(async () => {
+	return new Promise((resolve, reject) => {
+		map.render(center, zoom)
+		.then(async () => {
 
-		const imageBuffer = await map.image.buffer('image/png');
+			const imageBuffer = await map.image.buffer('image/png');
 
-		const uploadParams = { Bucket: process.env.BUCKET, Key: '', Body: '', ContentType: 'image/png', };
+			const uploadParams = { Bucket: process.env.BUCKET, Key: '', Body: '', ContentType: 'image/png', };
 
-		uploadParams.Body = imageBuffer;
-		uploadParams.Key = mapImage;
+			uploadParams.Body = imageBuffer;
+			uploadParams.Key = mapImage;
 
-		s3.upload(uploadParams, function (err, data) {
-			if (err) {
-				res.json({
-					status: 'error',
-					err: err,
-				});
-			}
+			s3.upload(uploadParams, function (err, data) {
+				if (err) {
+					reject(err);
+				}
 
-			if (data) {
-				res.json({
-					status: 'success',
-					url: data.Location,
-				});
+				if (data) {
+					resolve(data.Location);
+				}
+			});
+		})
+		.catch((err) => reject(err));
+	});
+};
+
+app.get('/generate', async (req, res) => {
+	const { query } = req;
+
+	const { lat, lng, zoom = 16 } = query;
+
+	try {
+		const smallLink = await generateMap(lat, lng, 'small', zoom);
+		const mediumLink = await generateMap(lat, lng, 'medium', zoom);
+		const largeLink = await generateMap(lat, lng, 'large', zoom);
+
+		res.json({
+			status: 'success',
+			versions: {
+				small: smallLink,
+				medium: mediumLink,
+				large: largeLink,
 			}
 		});
-	})
-	.catch((err) => res.json({
-		status: 'Something went wrong',
-		err,
-	}));
+	} catch (err) {
+		console.log(err);
+	}
+
 });
 
 const PORT = process.env.PORT || 3000;
